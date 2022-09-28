2c138c51a59b41311e45d485e1e9f122d4272683
Nicole Caceres <janenieggcs@gmail.com>
2022-09-27 21:54:41 -0500

    Agregar archivos de proyecto.

---
+++ AppS4/AppS4/ImageResourceExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppS4
+{
+ 
+    [Preserve(AllMembers = true)]
+    [ContentProperty(nameof(Source))]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+                return null;
+            //Do your translation lookup here, using whatever method you
+            var imageSource = ImageSource.FromResource(Source,
+                typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+            return imageSource;
+        }
+    }
+}
